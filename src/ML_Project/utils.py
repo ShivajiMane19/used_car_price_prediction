164e9d0de9d997bb5811494a4d32bb46014f6ea1
Shivaji Mane <maneshiva92@gmail.com>
2024-08-07 02:07:15 +0530

    data ingestion incomplete

---
+++ src/ML_Project/utils.py
@@ -0,0 +1,37 @@
+# For generic functionality
+
+import os
+import sys
+import pandas as pd
+from src.ML_Project.exception import CustomException
+from src.ML_Project.logger import logging
+
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv("db")
+
+def read_sql_data():
+    logging.info("Reading MySQL database started.")
+    try:
+        mydb = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db
+        )
+        logging.info("Connection established...", mydb)
+
+        # Now reading the dataset
+        df = pd.read_sql_query("select * from car_price", mydb)
+        print(df.head())
+
+        return df
+
+    except Exception as e:
+        raise CustomException(sys, e)
